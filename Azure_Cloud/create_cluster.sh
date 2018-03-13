42fbff166e32700dbabdf4eea4cf9bda5e73a221
cihanb <johndoe@example.com>
2018-03-13 10:09:47 -0700

    supressed additional debug msg

--- Azure_Cloud/create_cluster.sh
+++ Azure_Cloud/create_cluster.sh
@@ -29,26 +29,32 @@ source ./my_private_settings.sh
 
 # # login required
 echo $info_color"INFO"$no_color": First we need to log you in."
-az login -u $azure_account
+az login -u $azure_account > /dev/null
 
 # # create resource group
 echo $info_color"INFO"$no_color": Creating the resource group "$resource_group_name" in "$location
-az group create --name $resource_group_name --location $location
+echo $info_color"INFO"$no_color": az group create --name $resource_group_name --location $location"
+az group create --name $resource_group_name --location $location > /dev/null
 
 # # create aks cluster
 echo $info_color"INFO"$no_color": Creating the cluster "$aks_cluster_name
+echo $info_color"INFO"$no_color": az aks create --resource-group $resource_group_name  --name $aks_cluster_name --node-count $rp_total_nodes --generate-ssh-keys"
 az aks create --resource-group $resource_group_name  --name $aks_cluster_name --node-count $rp_total_nodes --generate-ssh-keys
 
 # #get aks credentials
 echo $info_color"INFO"$no_color": Save credentials"
+echo $info_color"INFO"$no_color": az aks get-credentials --resource-group $resource_group_name --name $aks_cluster_name"
 az aks get-credentials --resource-group $resource_group_name --name $aks_cluster_name
 
 # #create public IP
 echo $info_color"INFO"$no_color": create a public IP"
+echo $info_color"INFO"$no_color": az network public-ip create --resource-group $rg --name redis-ip --allocation-method static"
 rg="MC_"$resource_group_name"_"$aks_cluster_name"_"$location
 az network public-ip create --resource-group $rg --name redis-ip --allocation-method static
 cmd="az network public-ip list --resource-group $rg --query [0].ipAddress --output tsv"
 ip=$(eval $cmd)
 
 # #deploy redis enterpise
+echo $info_color"INFO"$no_color": deploy redis enterprise"
+echo $info_color"INFO"$no_color": kubectl create -f redis-enterprise.yaml"
 sed -e "s/\${ip}/$ip/" -e "s/\${image}/$rp_image/" redis-enterprise.yaml | kubectl create -f -
