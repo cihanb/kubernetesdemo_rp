bebc8c45c21cf639a55a4fcce24d564eebfb2704
Cihan B <cihanb@users.noreply.github.com>
2018-03-19 08:25:43 -0700

    Dockerk8demo (#3)
    * updated Azure_cloud deployment scripts
    
    * better confirmation on delete
    
    * formatting changes
    
    * adding the new files for the headless service and RBAC in k8
    
    * supressed additional debug msg
    
    * added gitignore
    
    * added clarity in debug
    
    * Delete my_private_settings.sh

--- Azure_Cloud/create_cluster.sh
+++ Azure_Cloud/create_cluster.sh
@@ -29,26 +29,37 @@ source ./my_private_settings.sh
 
 # # login required
 echo $info_color"INFO"$no_color": First we need to log you in."
-az login -u $azure_account
+az login -u $azure_account > /dev/null
 
-# # create resource group
+# create resource group
 echo $info_color"INFO"$no_color": Creating the resource group "$resource_group_name" in "$location
-az group create --name $resource_group_name --location $location
+echo $info_color"INFO"$no_color": az group create --name $resource_group_name --location $location"
+echo ""
+az group create --name $resource_group_name --location $location > /dev/null
 
-# # create aks cluster
+# create aks cluster
 echo $info_color"INFO"$no_color": Creating the cluster "$aks_cluster_name
+echo $info_color"INFO"$no_color": az aks create --resource-group $resource_group_name  --name $aks_cluster_name --node-count $rp_total_nodes --generate-ssh-keys"
+echo ""
 az aks create --resource-group $resource_group_name  --name $aks_cluster_name --node-count $rp_total_nodes --generate-ssh-keys --node-vm-size $rp_vm_sku
 
-# #get aks credentials
+# get aks credentials
 echo $info_color"INFO"$no_color": Save credentials"
+echo $info_color"INFO"$no_color": az aks get-credentials --resource-group $resource_group_name --name $aks_cluster_name"
+echo ""
 az aks get-credentials --resource-group $resource_group_name --name $aks_cluster_name
 
-# #create public IP
+# create public IP
 echo $info_color"INFO"$no_color": create a public IP"
+echo $info_color"INFO"$no_color": az network public-ip create --resource-group $rg --name redis-ip --allocation-method static"
+echo ""
 rg="MC_"$resource_group_name"_"$aks_cluster_name"_"$location
 az network public-ip create --resource-group $rg --name redis-ip --allocation-method static
 cmd="az network public-ip list --resource-group $rg --query [0].ipAddress --output tsv"
 ip=$(eval $cmd)
 
-# #deploy redis enterpise
+# deploy redis enterpise
+echo $info_color"INFO"$no_color": deploy redis enterprise"
+echo $info_color"INFO"$no_color": kubectl create -f redis-enterprise.yaml"
+echo ""
 sed -e "s/\${ip}/$ip/" redis-enterprise.yaml | kubectl create -f -
